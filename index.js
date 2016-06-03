a9d26a7b7aad93d4d16000498426ec3a1690e10d
Mark F. Ellis II <ellismarkf@gmail.com>
2016-06-03 02:08:38 -0500

    add blue section img content and interactions

--- index.js
+++ index.js
@@ -23,22 +23,23 @@ pinAnimations
 			{ css: { bottom: window.innerHeight }}
 		)
 	)
-	// .append(
-	// 	TweenMax.fromTo($('#running'), 2,
-	// 		{ css: { bottom: 0}},
-	// 		{ css: { bottom: window.innerHeight }}
-	// 	)
-	// );
+	.append(
+		TweenMax.fromTo($('#running'), 2,
+			{ css: { bottom: 0}},
+			{ css: { bottom: window.innerHeight }}
+		)
+	);
 
 // // pin element, use onPin and onUnpin to adjust the height of the element
 controller.pin($('#landing'), 1000, {
 	anim:pinAnimations, 
 	onPin: function() {
-		console.log('pinning')
 		$('.markets-links a').removeClass();
+		$('#hiddenMsg').css({ position: 'fixed' });
 	}, 
 	onUnpin: function() {
 		$('.markets-links a').addClass('dark');
+		$('#hiddenMsg').css({ position: 'absolute' });
 		// revert #landing to position:relative, remove height specificaiton
 	}
 });
@@ -51,3 +52,9 @@ window.addEventListener('scroll', function() {
 		$('.markets-links a').addClass('dark');
 	}
 })
+
+controller.addTween('#row-1', TweenMax.from( $('#row-1'), .25, {css:{right:'2000px'}, ease:Quad.easeInOut}));
+controller.addTween('#row-2', TweenMax.from( $('#row-2'), .25, {css:{left:'2000px'}, ease:Quad.easeInOut}));
+controller.addTween('#row-3', TweenMax.from( $('#row-3'), .25, {css:{right:'2000px'}, ease:Quad.easeInOut}));
+controller.addTween('#row-4', TweenMax.from( $('#row-4'), .25, {css:{left:'2000px'}, ease:Quad.easeInOut}));
+controller.addTween('#YOU', TweenMax.to( $('#YOU'), .25, {css:{color:'#183F67'}, ease:Quad.easeInOut}));
