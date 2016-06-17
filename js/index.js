eb19f6f10c557a5d0b794942535c13d16d9d807e
Mark F. Ellis II <ellismarkf@gmail.com>
2016-06-16 23:58:54 -0500

    updates based on feedback

--- js/index.js
+++ js/index.js
@@ -66,9 +66,10 @@ controller.addTween('#row-2', TweenMax.from( $('#row-2'), .25, {css:{left:'2000p
 controller.addTween('#row-3', TweenMax.from( $('#row-3'), .25, {css:{right:'2000px'}, ease:Quad.easeInOut}));
 controller.addTween('#row-4', TweenMax.from( $('#row-4'), .25, {css:{left:'2000px'}, ease:Quad.easeInOut}));
 
+controller.addTween('#CAREER', TweenMax.to( $('#CAREER'), .25, {css:{color:'#54A5D9'}, ease:Quad.easeInOut}));
 controller.addTween('#DO', TweenMax.to( $('#DO'), .25, {css:{color:'#54A5D9'}, ease:Quad.easeInOut}));
 controller.addTween('#YOU', TweenMax.to( $('#YOU'), .25, {css:{color:'#54A5D9'}, ease:Quad.easeInOut}));
-controller.addTween('#WE', TweenMax.to( $('#WE'), .25, {css:{color:'#183F67'}, ease:Quad.easeInOut}));
+controller.addTween('#INTERACT', TweenMax.to( $('#INTERACT'), .25, {css:{color:'#54A5D9'}, ease:Quad.easeInOut}));
 controller.addTween('#CONTACT', TweenMax.to( $('#CONTACT'), .25, {css:{color:'#000C34'}, ease:Quad.easeInOut}));
 
 
--- submit_handler.php
+++ submit_handler.php
@@ -1,11 +1,13 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "info@group206.com"; 
+    // $to = "info@group206.com";
+    $to = "ellismarkf@gmail.com";
     $from = $_POST['email'];
     $name = $_POST['name'];
     $phone = $_POST['phone'];
+    $msg = $_POST['msg'];
     $subject = "Summit Client Contact Form Submission";
-    $message = "Name:" . "\n" . $name . "\n\n Email:" . $from . "\n\n Phone #:" . $phone;
+    $message = "Name:" . "\n" . $name . "\n\n Email:\n" . $from . "\n\n Phone #:\n" . $phone . "\n\n Message:\n" . $msg;
 
     $headers = "From:" . $from;
     mail($to,$subject,$message,$headers);
